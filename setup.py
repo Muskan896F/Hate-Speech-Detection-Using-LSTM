1e1ba69bd8fa6bc87b1b244da68b495eda79b7db
Muskan Ara <muskan17ara@gmail.com>
2025-06-20 11:46:20 +0530

    exception file

---
+++ setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import find_packages, setup
+
+
+setup(
+    name = "hate-speech-classification",
+    version="0.0.1",
+    author="Muskan Ara",
+    author_email="muskan17ara@gmail.com",
+    packages=find_packages(),
+    install_required = [],
+)
